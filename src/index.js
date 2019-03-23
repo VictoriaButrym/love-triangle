48411ea443541099f40a119c7645b24c4e3c7081
victoria <victoria-butrym@tut.by>
2019-03-23 15:50:38 +0300

    feat: add function which counts the number of love triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let total = 0;
+  let countTriangles = 0;
+  
+  for (let i = 0; i < preferences.length; i++ ){
+    let first = preferences[i];
+    let second = preferences[first - 1];
+    let third = preferences[second - 1];
+    if (third === i + 1){
+      countTriangles++;
+    }
+    if (countTriangles == 3){
+      total++;
+      countTriangles = 0;
+    }
+  }
+  return total;
 };
